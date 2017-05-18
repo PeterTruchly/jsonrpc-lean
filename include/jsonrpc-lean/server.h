cd62dd8500a37b4cf63292c465219142a0f73919
Peter Truchly <ptruchly@cpicardgroup.com>
2017-05-18 15:02:59 -0500

    adapted the service to datagram interfaces, added a feature of 'suspended' service at startup with requests qoing into queue - server has a backlog queue, this allows to connect as a client with service already attached but postpone the processing until  (for e.g.) the init is done... ... and got rid of the format/handler search altogether. what is the point of having 'jsonrpc 2.0' service handling anything else than json?

--- include/jsonrpc-lean/server.h
+++ include/jsonrpc-lean/server.h
@@ -23,23 +23,27 @@
 #include "request.h"
 #include "value.h"
 #include "fault.h"
-#include "formathandler.h"
-#include "jsonformathandler.h"
-#include "reader.h"
+#include "jsonreader.h"
 #include "response.h"
-#include "writer.h"
-#include "formatteddata.h"
+#include "jsonwriter.h"
 #include "jsonformatteddata.h"
 #include "dispatcher.h"
+#include "datagram_based.h"
 
-
-#include <string>
+#include <mutex>
+#include <atomic>
 
 namespace jsonrpc {
 
-    class Server {
+    class Server : public IDatagramConsumer {
     public:
-        Server() {}
+		explicit Server(IDatagramProducerConsumer & transmitter) :
+			m_transmitter(transmitter),
+			m_enabled(false)
+        {
+			transmitter.registerService(*this);
+        }
+
         ~Server() {}
 
         Server(const Server&) = delete;
@@ -47,51 +51,73 @@ namespace jsonrpc {
         Server(Server&&) = delete;
         Server& operator=(Server&&) = delete;
 
-        void RegisterFormatHandler(FormatHandler& formatHandler) {
-            myFormatHandlers.push_back(&formatHandler);
-        }
-
         Dispatcher& GetDispatcher() { return myDispatcher; }
+
+	    void datagramConsume(const std::string & message) override
+		{		
+			std::lock_guard<std::mutex> backlogLock(m_backlogMutex);
+
+			m_backlog.emplace_back(message);
+			processBacklog();
+		}
+
+		void enableService()
+        {
+			std::lock_guard<std::mutex> backlogLock(m_backlogMutex);
+
+			m_enabled = true;
+			processBacklog();
+        }
 
         // aContentType is here to allow future implementation of other rpc formats with minimal code changes
         // Will return NULL if no FormatHandler is found, otherwise will return a FormatedData
         // If aRequestData is a Notification (the client doesn't expect a response), the returned FormattedData will have an empty ->GetData() buffer and ->GetSize() will be 0
-        std::shared_ptr<jsonrpc::FormattedData> HandleRequest(const std::string& aRequestData, const std::string& aContentType = "application/json") {
-
-            // first find the correct handler
-            FormatHandler *fmtHandler = nullptr;
-            for (auto handler : myFormatHandlers) {
-                if (handler->CanHandleRequest(aContentType)) {
-                    fmtHandler = handler;
-                }
-            }
-
-            if (fmtHandler == nullptr) {
-                // no FormatHandler able to handle this request type was found
-                return nullptr;
-            }
-            
-            auto writer = fmtHandler->CreateWriter();
+        std::shared_ptr<jsonrpc::JsonFormattedData> HandleRequest(const std::string& aRequestData) const
+        {                        
+            auto writer = std::make_unique<JsonWriter>();
 
             try {
-                auto reader = fmtHandler->CreateReader(aRequestData);
+                auto reader = std::make_unique<JsonReader>(std::move(aRequestData));
                 Request request = reader->GetRequest();
                 reader.reset();
 
                 auto response = myDispatcher.Invoke(request.GetMethodName(), request.GetParameters(), request.GetId());
-                if (!response.GetId().IsBoolean() || response.GetId().AsBoolean() != false) {
-                    // if Id is false, this is a notification and we don't have to write a response
+                if (response.GetId()) // if Id is 0, this is a notification and we don't have to write a response
+				{
+                    
                     response.Write(*writer);
                 }
             } catch (const Fault& ex) {
-                Response(ex.GetCode(), ex.GetString(), Value()).Write(*writer);
+                Response(ex.GetCode(), ex.GetString(), 0).Write(*writer);
             }
 
             return writer->GetData();
         }
     private:
+		void processBacklog() //needs to be called when holding m_backlogMutex
+		{
+			if (m_enabled)
+			{
+				for (const auto & message : m_backlog)
+				{
+					datagramConsumeImpl_(message);
+				}
+
+				m_backlog.clear();
+			}
+		}
+
+		void datagramConsumeImpl_(const std::string & message) const
+		{
+			auto response = HandleRequest(message);
+			m_transmitter.datagramTransmit(response->GetData());
+		}
+
         Dispatcher myDispatcher;
-        std::vector<FormatHandler*> myFormatHandlers;
+		IDatagramTransmitter & m_transmitter;
+		std::atomic_bool m_enabled;
+		std::vector<std::string> m_backlog;
+		std::mutex m_backlogMutex;
     };
 
 } // namespace jsonrpc
